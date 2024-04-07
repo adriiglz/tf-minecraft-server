934e2a884a8556a2124058f1dd8dc78934cf61e5
adrián <UO287621@uniovi.es>
2024-04-07 15:43:06 +0200

    refactored some names

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 // Muestra la dirección IP de la instancia del servidor
 output "minecraft_server_ip" {
-  value = module.mc-server-instance.minecraft_server_ip
+  value = module.mc-server-module.minecraft_server_ip
 }
